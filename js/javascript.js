290511a0d82f1f1b011cbfb1e940320a871df271
Alexander Laudino <93599746+alexander-laudino@users.noreply.github.com>
2022-05-31 12:04:12 -0400

    Add event listeners for mouseover to all box divs

--- js/javascript.js
+++ js/javascript.js
@@ -5,7 +5,12 @@ let numBoxes = 16 * 16;
 for (let i = 0; i < numBoxes; i++) {
   let box = document.createElement("div");
   box.setAttribute("class", "box");
+  box.setAttribute("id", `box${i}`);
   container.appendChild(box);
 }
 
 const boxes = document.querySelectorAll(".box");
+
+boxes.forEach((box) => {
+  box.addEventListener("mouseover", (e) => console.log(e.target));
+});

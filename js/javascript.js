c7ac787e935416cb4b2fa0eb925942061913e745
Alexander Laudino <93599746+alexander-laudino@users.noreply.github.com>
2022-05-31 11:32:14 -0400

    Make 16 x 16 grid of divs

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,11 @@
 const container = document.getElementById("container");
+
+let numBoxes = 16 * 16;
+
+for (let i = 0; i < numBoxes; i++) {
+  let box = document.createElement("div");
+  box.setAttribute("class", "box");
+  container.appendChild(box);
+}
+
+const boxes = document.querySelectorAll(".box");
